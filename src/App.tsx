a786864b01a440bfb6f07e97ef39e74cf41d717a
unknown <sharipov.r@mail.ru>
2023-03-27 21:26:34 +0400

    3

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import './App.css';
 import MainContainer from './components/main/MainContainer';
 import ResultContainer from './components/result/ResultContainer';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, NavLink } from 'react-router-dom';
 
 function App() {
 
