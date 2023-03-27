54f74c6e51771615f2305ef683cbd980c7eec42f
unknown <sharipov.r@mail.ru>
2023-03-27 22:24:29 +0400

    5

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,20 @@
 import './App.css';
 import MainContainer from './components/main/MainContainer';
 import ResultContainer from './components/result/ResultContainer';
-import { BrowserRouter as Router, Routes, Route, NavLink } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
 
   return (
     <div className="App">
-      <Router>
-        <Routes>
-          <Route path="/" element={<MainContainer />} />
-          <Route path="/result" element={<ResultContainer />} />
-        </Routes>
-      </Router>
+      
+        <Router basename={process.env.PUBLIC_URL}>
+          <Routes>
+            <Route path="/" element={<MainContainer />} />
+            <Route path="/result" element={<ResultContainer />} />
+          </Routes>
+        </Router>
+
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,16 @@ import App from './App';
 import { store } from './store/index';
 import { Provider } from 'react-redux'
 
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+
+    <Provider store={store}>
+      <App />
+    </Provider>
+
 );
 
 
